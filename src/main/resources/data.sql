49e6d78affbba64a01b10f859bd8e7a81aee6700
Mohnish <mohnish57920@gmail.com>
2023-09-05 11:13:43 +0530

    updated app props yet again 9

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,9 +1,9 @@
-INSERT INTO candidate(id, name, email, salary, expense) VALUES (1, 'JACK', 'jack@email.com', 20000, 14000);
-INSERT INTO candidate(id, name, email, salary, expense) VALUES (2, 'BOB', 'bob@email.com', 33000, 4000);
-INSERT INTO candidate(id, name, email, salary, expense) VALUES (3, 'JIM', 'jim@email.com', 22000, 13000);
-INSERT INTO candidate(id, name, email, salary, expense) VALUES (4, 'ROSE', 'rose@email.com', 28000, 7000);
-INSERT INTO candidate(id, name, email, salary, expense) VALUES (5, 'ALICE', 'alice@email.com', 40000, 22000);
-INSERT INTO candidate(id, name, email, salary, expense) VALUES (6,'TIM', 'tim@email.com', 26000, 21000);
-INSERT INTO candidate(id, name, email, salary, expense) VALUES (7, 'STEVE', 'steve@email.com', 20000, 5000);
-
-
+--INSERT INTO candidate(id, name, email, salary, expense) VALUES (1, 'JACK', 'jack@email.com', 20000, 14000);
+--INSERT INTO candidate(id, name, email, salary, expense) VALUES (2, 'BOB', 'bob@email.com', 33000, 4000);
+--INSERT INTO candidate(id, name, email, salary, expense) VALUES (3, 'JIM', 'jim@email.com', 22000, 13000);
+--INSERT INTO candidate(id, name, email, salary, expense) VALUES (4, 'ROSE', 'rose@email.com', 28000, 7000);
+--INSERT INTO candidate(id, name, email, salary, expense) VALUES (5, 'ALICE', 'alice@email.com', 40000, 22000);
+--INSERT INTO candidate(id, name, email, salary, expense) VALUES (6,'TIM', 'tim@email.com', 26000, 21000);
+--INSERT INTO candidate(id, name, email, salary, expense) VALUES (7, 'STEVE', 'steve@email.com', 20000, 5000);
+--
+--
